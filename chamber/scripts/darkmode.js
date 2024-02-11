16d6c8a3d1485705a8138a60bb5068b66a2fe3c7
Isaac Huffman <122467839+isaachuffman37@users.noreply.github.com>
2024-02-10 18:51:27 -0700

    added dark mode button

---
+++ chamber/scripts/darkmode.js
@@ -0,0 +1,34 @@
+const modeButton = document.querySelector("#dark-mode");
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
+const header = document.querySelector("header");
+const navigationLinks = document.querySelectorAll("nav a");
+
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("Dark")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+        footer.style.background = "#000";
+        footer.style.color = "#fff";
+        header.style.background = "#000";
+        header.style.color = "#fff";
+        navigationLinks.forEach(link => {
+            link.style.background = "#000"
+            link.style.color = "#fff"
+        });
+		modeButton.textContent = "Light";
+	} else {
+		main.style.background = "#ebf2fa";
+		main.style.color = "#000";
+        footer.style.background = "#013a63";
+        footer.style.color = "#fff";
+        header.style.background = "#013a63";
+        header.style.color = "#fff";
+        navigationLinks.forEach(link => {
+            link.style.background = "#ebf2fa"
+            link.style.color = "#013a63"
+        });
+		modeButton.textContent = "Dark";
+	}
+});
