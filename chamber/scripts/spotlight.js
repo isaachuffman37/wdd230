a00b654267da1191b709d65a95a378e44d3ab6c8
Isaac Huffman <122467839+isaachuffman37@users.noreply.github.com>
2024-03-27 00:52:32 -0600

    made some good changes

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -1,42 +1,48 @@
 const membersURL = "https://isaachuffman37.github.io/wdd230/chamber/data/members.json";
-const mainGridContainer = document.querySelector(".directory-grid");
+const spotlightDiv = document.querySelector(".spotlight-company");
+var highMembers = [];
 async function getMembers() {
     const response = await fetch(membersURL);
     const data = await response.json();
-    //console.log(data.companies)
-    displayMembers(data.companies)
+    console.log(data.companies)
+    displayMember(data.companies)
 }
 
-function displayMembers (members) {
+function displayMember (members) {
     members.forEach((member)=>{
-        let sectionElement = document.createElement('section');
-        let memberName = document.createElement('h2');
-        let address = document.createElement('p');
-        let phone = document.createElement('p');
-        let website = document.createElement('a');
-        let membershipLevel = document.createElement('p');
-        let companyImage = document.createElement('img');
-        
-        memberName.textContent = `${member.name}`
-        address.textContent = `${member.address}`
-        phone.textContent = `${member.phone}`
-        website.textContent = `${member.website}`
-        membershipLevel.textContent = `${member.membership_level}`
+        if(member.membership_level == 'Silver Membership' || member.membership_level == 'Gold Membership'){
+            highMembers.push(member);
+        }
+    });
+
+    let randomIndex = Math.floor(Math.random() * highMembers.length);
+    let member = highMembers[randomIndex];
+    let sectionElement = document.createElement('section');
+    let memberName = document.createElement('h2');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let website = document.createElement('a');
+    let membershipLevel = document.createElement('p');
+    let companyImage = document.createElement('img');
+    
+    memberName.textContent = `${member.name}`
+    address.textContent = `${member.address}`
+    phone.textContent = `${member.phone}`
+    website.textContent = `${member.website}`
+    membershipLevel.textContent = `${member.membership_level}`
 
 
-        companyImage.setAttribute('src', `${member.image}`);
-        sectionElement.setAttribute('class', "member-grid");
-        website.setAttribute('href', `${member.website}`);
+    companyImage.setAttribute('src', `${member.image}`);
+    sectionElement.setAttribute('class', "member-grid");
+    website.setAttribute('href', `${member.website}`);
 
-        sectionElement.appendChild(companyImage);
-        sectionElement.appendChild(memberName);
-        sectionElement.appendChild(address);
-        sectionElement.appendChild(phone);
-        sectionElement.appendChild(website);
-        sectionElement.appendChild(membershipLevel);
-        mainGridContainer.appendChild(sectionElement)
-
-    })
+    sectionElement.appendChild(companyImage);
+    sectionElement.appendChild(memberName);
+    sectionElement.appendChild(address);
+    sectionElement.appendChild(phone);
+    sectionElement.appendChild(website);
+    sectionElement.appendChild(membershipLevel);
+    spotlightDiv.appendChild(sectionElement)
 }
 
 getMembers()
